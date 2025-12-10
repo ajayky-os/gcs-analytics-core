2ee049c41cd1041327f8fc3f5d9056d513dd8707
Ajay Yadav <ajayky@google.com>
2025-12-10 12:49:13 +0000

    add microbenchmark reusing filesystem

--- core/src/jmh/java/com/google/cloud/gcs/analyticscore/core/JsonVsGrpcBenchmark.java
+++ core/src/jmh/java/com/google/cloud/gcs/analyticscore/core/JsonVsGrpcBenchmark.java
@@ -20,6 +20,8 @@ import java.net.URI;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import com.google.cloud.gcs.analyticscore.client.GcsFileSystem;
+import com.google.cloud.gcs.analyticscore.client.GcsFileSystemImpl;
 import com.google.cloud.gcs.analyticscore.client.GcsFileSystemOptions;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
@@ -27,10 +29,15 @@ import org.openjdk.jmh.infra.Blackhole;
 
 @State(Scope.Benchmark)
 public class JsonVsGrpcBenchmark {
+  GcsFileSystem grpcFileSystem;
+  GcsFileSystem jsonFileSystem;
 
   @Setup(Level.Trial)
   public void uploadSampleFiles() throws IOException {
     IntegrationTestHelper.uploadSampleParquetFilesIfNotExists();
+    grpcFileSystem = new GcsFileSystemImpl(GcsFileSystemOptions.createFromOptions(
+            Map.of("gcs.client.type", "GRPC_CLIENT"), "gcs."));
+    jsonFileSystem = new GcsFileSystemImpl(GcsFileSystemOptions.builder().build());
   }
 
   @Benchmark
@@ -45,6 +52,21 @@ public class JsonVsGrpcBenchmark {
     URI uri = IntegrationTestHelper.getGcsObjectUriForFile(state.fileSize);
 
     long recordCount = ParquetHelper.readParquetObjectRecords(uri, /* readVectoredEnabled= */ true, gcsFileSystemOptions);
+
+    blackhole.consume(recordCount);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  @Warmup(iterations = 1, time = 1)
+  @Measurement(iterations = 2, time = 1)
+  @Fork(value = 2, warmups = 1)
+  public void readParquetRecordsReusingFileSystem(JsonVsGrpcState state, Blackhole blackhole) throws IOException {
+    GcsFileSystem fileSystem = state.clientType.equals("GRPC_CLIENT") ? grpcFileSystem : jsonFileSystem;
+    URI uri = IntegrationTestHelper.getGcsObjectUriForFile(state.fileSize);
+
+    long recordCount = ParquetHelper.readParquetObjectRecords(uri, /* readVectoredEnabled= */ true, fileSystem);
 
     blackhole.consume(recordCount);
   }
